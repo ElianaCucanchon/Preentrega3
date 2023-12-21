c9f185557442b0037856273a6fcdd1da77434259
ElianaCucanchon <146408300+ElianaCucanchon@users.noreply.github.com>
2023-12-21 10:24:54 -0500

    Add files via upload

---
+++ prendas.js
@@ -0,0 +1,74 @@
+const prendas = [
+    {
+        id: 1,
+        nombre: "Camisas",
+        img: "../img/indeximg10.jpg",
+        precio: "35.00",
+    },
+    {
+        id: 2,
+        nombre: "Joodies",
+        img: "../img/img8.webp",
+        precio: "30.00",
+    },
+    {
+        id: 3,
+        nombre: "Gorras",
+        img: "../img/nosotros/nosotros4.jpeg",
+        precio: "25.00",
+    },
+    {
+        id: 4,
+        nombre: "Personalizacion",
+        img: "../img/img8.webp",
+        precio: "45.00",
+    },
+    {
+        id: 5,
+        nombre: "Estampado",
+        img: "../img/Mujer/gorram5.webp",
+        precio: "40.00",
+    },
+    {
+        id: 6,
+        nombre: "Camisas rojas",
+        img: "../img/indeximg11.webp",
+        precio: "30.00",
+    },
+    {
+        id: 7,
+        nombre: "Joodies Azul",
+        img: "../img/joodieh1.webp",
+        precio: "40.00",
+    },
+    {
+        id: 8,
+        nombre: "Gorras Blancas",
+        img: "../img/hombre/gorrah1.webp",
+        precio: "45.00",
+    },
+    {
+        id: 9,
+        nombre: "Pareja",
+        img: "../img/hombreymujer.jpg",
+        precio: "25.00",
+    },
+    {
+        id: 10,
+        nombre: "Camisas-corazon-rosa",
+        img: "../img/img1.webp",
+        precio: "30.00",
+    },
+    {
+        id: 11,
+        nombre: "Joodies Claro",
+        img: "../img/img6.jpg",
+        precio: "40.00",
+    },
+    {
+        id: 12,
+        nombre: "Gorras Corazon",
+        img: "../img/img7.webp",
+        precio: "35.00",
+    }
+]
